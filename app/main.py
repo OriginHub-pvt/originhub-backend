666b3f6aa8e808f44bf5b35afbfedf05d2bd735f
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2025-12-11 08:14:40 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=cors_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     # allow_headers=["Content-Type", "Authorization"],
